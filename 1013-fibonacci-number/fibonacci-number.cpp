5b5f913d375457f8c4ed0a4235c514862225dea6
niketarora <niketarora682@gmail.com>
2025-12-13 22:47:27 +0530

    Time: 2 ms (76.21%) | Memory: 7.7 MB (87.48%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+    // METHOD - 1 without dp
+    // int fib(int n) {
+    //     if(n==0|| n==1) return n;
+    //     return fib(n-1) + fib(n-2);
+    // }
+
+    // METHOD - 2 with dp and memorization
+    // int fibo(int n, vector<int> &dp) {
+    //     if (n <= 1) return n;
+    //     if (dp[n] != -1) return dp[n];
+    //     return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+    // }
+    // int fib(int n) {
+    //     vector<int> dp(n + 1, -1);
+    //     return fibo(n, dp);
+    // }
+
+    // METHOD - 3 with tabulation(dp -> bottom up)
     int fib(int n) {
-        if(n==0|| n==1) return n;
-        return fib(n-1) + fib(n-2);
+        if (n <= 1) return n;
+
+        int dp[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
-};
+};  
