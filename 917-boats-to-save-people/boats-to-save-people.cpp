e4c2932c9107755a82613f0256920572d8c38bea
niketarora <niketarora682@gmail.com>
2025-12-13 21:54:22 +0530

    Time: 19 ms (48.92%) | Memory: 45.9 MB (5.44%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i = 0; 
+        int j = people.size() - 1;
+        int res = 0;
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                res++;
+                i++;
+                j--;
+            }
+            else{
+                res++;
+                j--;
+            }
+        }
+        return res;
+    }
+};
