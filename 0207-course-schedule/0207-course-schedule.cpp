da8baef1cc96d3b29e65ecb29e8edcde45a96999
niketarora <niketarora682@gmail.com>
2026-01-28 18:01:43 +0530

    Time: 6 ms (52.77%), Space: 20.1 MB (36.56%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(V);
+        for(auto e : prerequisites){
+            adj[e[0]].push_back(e[1]);
+        }
+        
+        vector<int> inDegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(auto it : adj[i]){
+                inDegree[it]++;
+            }
+        }
+
+
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it : adj[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+
+        if(topo.size() == V) return true;
+        else return false;
+    }
+};
